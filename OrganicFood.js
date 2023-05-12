dd5aded0bf7f1021353a326bc1ffbc39161424fb
Natalia Emelianova <128471188+NataliaEmelia@users.noreply.github.com>
2023-05-12 10:19:08 +0200

    Update OrganicFood.js

--- OrganicFood.js
+++ OrganicFood.js
@@ -6,20 +6,56 @@ $(document).ready(function() {
     });
 });
 
-app.use(express.static('public', {
-  etag: true, // Just being explicit about the default.
-  lastModified: true,  // Just being explicit about the default.
-  setHeaders: (res, path) => {
-    const hashRegExp = new RegExp('\\.[0-9a-f]{8}\\.');
+"use strict";
 
-    if (path.endsWith('.html')) {
-      // All of the project's HTML files end in .html
-      res.setHeader('Cache-Control', 'no-cache');
-    } else if (hashRegExp.test(path)) {
-      // If the RegExp matched, then we have a versioned URL.
-      res.setHeader('Cache-Control', 'max-age=31536000');
+var imageCache = new function() {
+  var me = this;
+
+  var cache = [];
+  var root = document.location.href.split("/");
+
+  root.pop();
+  root = root.join("/") + "/";
+
+  me.push = function(src, loadEvent) {
+    if (!src.match(/^http/)) {
+      src = root + src;
     }
-  },
-}));
 
+    var item = new Image();
+
+    if (cache[src] && loadEvent) {
+      loadEvent(src);
+    } else {
+      if (loadEvent) {
+        item.onload = loadEvent;
+        item.onerror = loadEvent;
+      }
+      cache[src] = item;
+    }
+
+    item.src = src;
+  };
+
+  me.pushArray = function(array, imageLoadEvent, imagesLoadEvent) {
+    var numLoaded = 0;
+    var arrayLength = array.length;
+    for (var i = 0; i < arrayLength; i++) {
+      me.push(array[i], function(e) {
+        if (imageLoadEvent) {
+          imageLoadEvent(e);
+        }
+        numLoaded++;
+        if (numLoaded == arrayLength) {
+          setTimeout(function() {
+            imagesLoadEvent(e);
+          }, 1);
+        }
+      });
+    }
+  };
+}();
+
+var s = ["https://upload.wikimedia.org/wikipedia/commons/8/88/Half-Life_Black_Mesa_logo.svg", "https://upload.wikimedia.org/wikipedia/commons/5/5d/Clojure_logo.svg", "https://4hands.ru/wp-content/uploads/2018/04/viber-logo-256x256.png"];
+imageCache.pushArray(s, () => console.log('when one of all images has been loaded'), () => console.log('when all images have been loaded'));
 
